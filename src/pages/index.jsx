2a6812681d774bb51b0a9aadb958ac69c47c5dc8
twilightscapes <ihatespam@gmail.com>
2021-07-02 07:24:37 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -140,7 +140,7 @@ export default function IndexPage() {
 
 <ScrollAnimation animateIn="bounceInDown" delay={1350} offset={0}  initiallyVisible={false} animateOnce={true} animatePreScroll={true} style={{position:'relative', top:'0px', paddingRight:'10%', border:'0px solid yellow'}}>
 <h2
-  className="letter normal txtshadow-header hit-the-floor"
+  className="normal txtshadow-header hit-the-floor"
   style={{
     color: '#fff',
     fontSize: '60px',
@@ -178,7 +178,7 @@ on the
 
 <ScrollAnimation animateIn="bounceInUp" delay={1450} initiallyVisible={false} animateOnce={true} animatePreScroll={true} style={{position:'relative', top:'', paddingRight:'10%', border:'0px solid blue'}}>
 <h2
-  className="letter narrow txtshadow mobile-txt"
+  className="letter txtshadow-header narrow mobile-txt"
   style={{
    fontSize:'80px',
    color: 'white',
